68cbb8507d5e19407f49849b1db50a9af5d22157
oanatopan <19oanatopan@gmail.com>
2026-01-22 04:25:49 +0200

    modify java version on github

--- src/test/java/pages/BasePage.java
+++ src/test/java/pages/BasePage.java
@@ -4,11 +4,13 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.support.PageFactory;
 
 public class BasePage {
-    public WebDriver driver;
+    protected WebDriver driver;
+
 
     public BasePage(WebDriver driver) {
         this.driver = driver;
         PageFactory.initElements(driver, this
         );
     }
+
 }
